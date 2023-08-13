a246da56b8473bf5fcb0adf53a69f604f9f0d943
Jayvee Bustarde <jayvee.bustarde@gmail.com>
2023-08-13 17:58:13 +0200

    fixed scrolling

--- src/components/MainWrapper/MainWrapper.js
+++ src/components/MainWrapper/MainWrapper.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Stack } from "react-bootstrap";
 import "../MainWrapper/MainWrapper.scss";
+import ScrollToTop from "../ScrollToTop/ScrollToTop";
 
 const MainWrapper = ({ children }) => {
     return (
         <Stack gap={0} className="main-wrapper">
+            <ScrollToTop />
             {children}
         </Stack>
     );
